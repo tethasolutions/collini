7ba458167935ce4d1ad6fe133b8cb5afa09c648e
Gabriele Crivellari <gabriele.crivellari@itcoregroup.com>
2023-06-30 16:32:08 +0200

    Implementazione servizi clienti e indirizzi (Mirko)

--- src/Collini.GestioneInterventi.Application/ApplicationConfiguration.cs
+++ src/Collini.GestioneInterventi.Application/ApplicationConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Collini.GestioneInterventi.Application.Security;
+﻿using Collini.GestioneInterventi.Application.Customers.Services;
+using Collini.GestioneInterventi.Application.Security;
 using Collini.GestioneInterventi.Application.Session;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,7 +13,9 @@ public static class ApplicationConfiguration
         services
             .AddScoped<ISecurityContextFactory, SecurityContextFactory>()
             .AddScoped<ISecurityService, SecurityService>()
-            .AddScoped<IAccessTokenProvider, TAccessTokenProvider>();
+            .AddScoped<IAccessTokenProvider, TAccessTokenProvider>()
+            .AddScoped<IContactService, ContactService>()
+            .AddScoped<IAddressService, AddressService>();
 
         return services;
     }
