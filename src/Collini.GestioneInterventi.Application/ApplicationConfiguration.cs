69ddabd3a123a1a6bc34a650538816990a0ec998
Gabriele Crivellari <gabriele.crivellari@itcoregroup.com>
2023-07-04 16:44:00 +0200

    .

--- src/Collini.GestioneInterventi.Application/ApplicationConfiguration.cs
+++ src/Collini.GestioneInterventi.Application/ApplicationConfiguration.cs
@@ -1,4 +1,8 @@
-﻿using Collini.GestioneInterventi.Application.Customers.Services;
+﻿using Collini.GestioneInterventi.Application.Activities.Services;
+using Collini.GestioneInterventi.Application.Customers.Services;
+using Collini.GestioneInterventi.Application.Jobs.Services;
+using Collini.GestioneInterventi.Application.Notes.Services;
+using Collini.GestioneInterventi.Application.Orders.Services;
 using Collini.GestioneInterventi.Application.Security;
 using Collini.GestioneInterventi.Application.Session;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,7 +19,11 @@ public static class ApplicationConfiguration
             .AddScoped<ISecurityService, SecurityService>()
             .AddScoped<IAccessTokenProvider, TAccessTokenProvider>()
             .AddScoped<IContactService, ContactService>()
-            .AddScoped<IAddressService, AddressService>();
+            .AddScoped<IAddressService, AddressService>()
+            .AddScoped<INotesService, NoteService>()
+            .AddScoped<IOrderService, OrderService>()
+            .AddScoped<IActivityService, ActivityService>()
+            .AddScoped<IJobService, JobService>();
 
         return services;
     }
